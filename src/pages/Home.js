f033ae4c127caf717b02da30de9a061dc1736c99
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2025-10-08 16:20:06 +0300

    bug fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import { Grid, GridItem } from "@chakra-ui/react";
 function Home() {
   return (
     <div>
-      <Grid templateColumns="repeat(4, 1fr)" gap="6">
+      <Grid templateColumns="repeat(3, 1fr)" gap="6">
         <GridItem colSpan={1}></GridItem>
         <GridItem colSpan={2}>
           <Navbar />
