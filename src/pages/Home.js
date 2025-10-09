eb291346ba1af3ef50b8b531a6bbf7d35235e3e6
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2025-10-09 10:14:41 +0300

    text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Search from "../components/Search";
+import ArrowImage from "../images/arrow.png";
 
 function Home() {
   return (
@@ -10,6 +11,13 @@ function Home() {
         <div className="col-span-1 md:col-span-3 flex flex-col ">
           <Navbar />
           <Search />
+          <div className="flex items-center justify-between mt-6 mb-4">
+            <div className="flex items-center">
+              <img src={ArrowImage} alt="Logo" className="h-09 w-09 mr-2" />
+              <span className="text-white font-bold text-xl">Trending Now</span>
+            </div>
+            <div>{/* Tabs */}</div>
+          </div>
         </div>
         <div className="hidden md:block"></div>
       </div>
