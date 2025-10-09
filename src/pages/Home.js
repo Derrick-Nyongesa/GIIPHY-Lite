d86bc29399ebea2d44975c3ed6d64e68758b0d70
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2025-10-09 07:57:02 +0300

    grid columns created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,14 @@ import Search from "../components/Search";
 function Home() {
   return (
     <div>
-      <Navbar />
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
+        <div className="hidden md:block"></div>
+        <div className="col-span-1 md:col-span-3 flex flex-col ">
+          <Navbar />
+          <Search />
+        </div>
+        <div className="hidden md:block"></div>
+      </div>
     </div>
   );
 }
