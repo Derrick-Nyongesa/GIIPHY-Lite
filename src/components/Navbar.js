fd7a98590a78853ad71450272b310d3df82071fa
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2025-10-14 07:57:05 +0300

    bug fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import LogoImage from "../images/logo.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div>
       <nav className="flex items-center justify-between p-2 ">
         <div className="flex items-center">
-          <img src={LogoImage} alt="Logo" className="h-12 w-12 mr-2" />
-          <span className="text-white font-bold text-4xl">GIPHY-Lite</span>
+          <Link to="/" className="flex items-center no-underline">
+            <img src={LogoImage} alt="Logo" className="h-12 w-12 mr-2" />
+            <span className="text-white font-bold text-4xl">GIPHY-Lite</span>
+          </Link>
         </div>
         <div>
           {/* <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
